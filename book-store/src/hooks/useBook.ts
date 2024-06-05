bc5cbee3cb262bab8b661b926471c68bedbd71fa
Eunji <eunjipark0129@gmail.com>
2024-06-05 18:39:11 +0900

    feat : 도서 상세 페이지 구현

---
+++ book-store/src/hooks/useBook.ts
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { fetchBook, likeBook, unlikeBook } from '../api/books.api';
+import { BookDetail } from '../models/book.model';
+import { useAuthStore } from '../store/authStore';
+import { useAlert } from './useAlert';
+import { addCart } from '../api/carts.api';
+
+export const useBook = (bookId: string | undefined) => {
+  const [book, setBook] = useState<BookDetail | null>(null);
+  const [cartAdded, setCartAdded] = useState(false);
+  const { isLoggedIn } = useAuthStore();
+  const { showAlert } = useAlert();
+
+  const addToCart = (quantity: number) => {
+    if (!book) return;
+
+    addCart({
+      bookId: book.id,
+      quantity,
+    }).then(() => {
+      setCartAdded(true);
+      setTimeout(() => {
+        setCartAdded(false);
+      }, 3000);
+    });
+  };
+
+  const likeToggle = () => {
+    //권한 확인
+    if (!isLoggedIn) {
+      showAlert('로그인이 필요한 기능입니다.');
+      return;
+    }
+
+    if (!book) return;
+
+    if (book.liked) {
+      unlikeBook(book.id).then(() => {
+        //낙관적 업데이트
+        setBook({
+          ...book,
+          liked: false,
+          likes: book.likes - 1,
+        });
+      });
+    } else {
+      likeBook(book.id).then(() => {
+        setBook({
+          ...book,
+          liked: true,
+          likes: book.likes + 1,
+        });
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (!bookId) return;
+
+    fetchBook(bookId).then((book) => {
+      setBook(book);
+    });
+  }, [bookId]);
+
+  return { book, likeToggle, cartAdded, addToCart };
+};
--- book-store/src/models/book.model.ts
+++ book-store/src/models/book.model.ts
@@ -16,6 +16,6 @@ export interface Book {
 }
 
 export interface BookDetail extends Book {
-  categoryName: string;
+  category: string;
   liked: boolean;
 }
