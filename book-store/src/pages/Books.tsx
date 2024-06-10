bb9a3e0d0cb47264e2ee059b5892896f8f5525c5
Eunji <eunjipark0129@gmail.com>
2024-06-10 19:48:42 +0900

    chore : craco 추가 및 path 설정

--- book-store/src/pages/Books.tsx
+++ book-store/src/pages/Books.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Title from '../components/common/Title';
+import Title from '@/components/common/Title';
 import styled from 'styled-components';
-import BooksFilter from '../components/books/BooksFilter';
-import BooksList from '../components/books/BooksList';
-import BooksEmpty from '../components/books/BooksEmpty';
-import Pagination from '../components/books/Pagination';
-import BooksViewSwitcher from '../components/books/BooksViewSwitcher';
-import { useBooks } from '../hooks/useBooks';
+import BooksFilter from '@/components/books/BooksFilter';
+import BooksList from '@/components/books/BooksList';
+import BooksEmpty from '@/components/books/BooksEmpty';
+import Pagination from '@/components/books/Pagination';
+import BooksViewSwitcher from '@/components/books/BooksViewSwitcher';
+import { useBooks } from '@/hooks/useBooks';
 
 export default function Books() {
   const { books, pagination, isEmpty } = useBooks();
