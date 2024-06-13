1e94b66c0921fb86b1b288a5fb2b6cfae698ca62
Eunji <eunjipark0129@gmail.com>
2024-06-13 13:20:31 +0900

    feat : DropDown UI 추가

--- book-store/src/components/common/Header.tsx
+++ book-store/src/components/common/Header.tsx
@@ -1,10 +1,12 @@
 import { styled } from 'styled-components';
 import logo from '../../assets/images/logo.png';
-import { FaSignInAlt, FaRegUser } from 'react-icons/fa';
+import { FaSignInAlt, FaRegUser, FaUserCircle } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCategory } from '../../hooks/useCategory';
 import { useAuthStore } from '../../store/authStore';
 import { QUERYSTRING } from '../../constants/querystring';
+import DropDown from './DropDown';
+import ThemeSwitcher from '../header/ThemeSwitcher';
 
 export default function Header() {
   const { category } = useCategory();
@@ -41,35 +43,40 @@ export default function Header() {
         </ul>
       </nav>
       <nav className='auth'>
-        {isLoggedIn && (
-          <ul>
-            <li>
-              <Link to='/cart'>장바구니</Link>
-            </li>
-            <li>
-              <Link to='/orderlist'>주문 내역</Link>
-            </li>
-            <li>
-              <button onClick={handleLogout}>로그아웃</button>
-            </li>
-          </ul>
-        )}
-        {!isLoggedIn && (
-          <ul>
-            <li>
-              <Link to='/login'>
-                <FaSignInAlt />
-                로그인
-              </Link>
-            </li>
-            <li>
-              <Link to='/signup'>
-                <FaRegUser />
-                회원가입
-              </Link>
-            </li>
-          </ul>
-        )}
+        <DropDown toggleButton={<FaUserCircle />}>
+          <>
+            {isLoggedIn && (
+              <ul>
+                <li>
+                  <Link to='/cart'>장바구니</Link>
+                </li>
+                <li>
+                  <Link to='/orderlist'>주문 내역</Link>
+                </li>
+                <li>
+                  <button onClick={handleLogout}>로그아웃</button>
+                </li>
+              </ul>
+            )}
+            {!isLoggedIn && (
+              <ul>
+                <li>
+                  <Link to='/login'>
+                    <FaSignInAlt />
+                    로그인
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/signup'>
+                    <FaRegUser />
+                    회원가입
+                  </Link>
+                </li>
+              </ul>
+            )}
+            <ThemeSwitcher />
+          </>
+        </DropDown>
       </nav>
     </HeaderStyle>
   );
@@ -113,7 +120,9 @@ const HeaderStyle = styled.header`
   .auth {
     ul {
       display: flex;
+      flex-direction: column;
       gap: 16px;
+      width: max-content;
       li {
         a,
         button {
@@ -122,6 +131,8 @@ const HeaderStyle = styled.header`
           text-decoration: none;
           display: flex;
           align-items: center;
+          justify-content: center;
+          width: max-content;
           line-height: 1.5;
           background: none;
           border: 0;
