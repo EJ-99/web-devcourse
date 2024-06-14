a07705e17a54f61b716b853bdb8eb5a574c9c871
Eunji <eunjipark0129@gmail.com>
2024-06-14 21:05:51 +0900

    feat : Home 페이지 신간 섹션

--- book-store/src/pages/Home.tsx
+++ book-store/src/pages/Home.tsx
@@ -1,16 +1,25 @@
+import Title from '@/components/common/Title';
+import MainNewBooks from '@/components/main/MainNewBooks';
 import MainReview from '@/components/main/MainReview';
 import { useMain } from '@/hooks/useMain';
 import styled from 'styled-components';
 
 export default function Home() {
-  const { reviews } = useMain();
+  const { reviews, newBooks } = useMain();
   return (
     <HomeStyle>
       {/* 배너 */}
       {/* 베스트셀러 */}
       {/* 신간 */}
+      <section className='section'>
+        <Title size='large'>신간 안내</Title>
+        <MainNewBooks books={newBooks} />
+      </section>
       {/* 리뷰 */}
-      <MainReview reviews={reviews} />
+      <section className='section'>
+        <Title size='large'>리뷰</Title>
+        <MainReview reviews={reviews} />
+      </section>
     </HomeStyle>
   );
 }
